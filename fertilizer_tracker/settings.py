e2dceafed6f3ad195253dbdddc0ae0503c4449a6
Yeshwanth Reddy Pingili <151595042+Jinja-max@users.noreply.github.com>
2025-08-20 10:15:04 +0530

    Update settings.py

--- fertilizer_tracker/settings.py
+++ fertilizer_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-js1=w+3r5w#)-1!n4m9can^ixlj_nc4q^6%aw_o_x6elr6v=^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
